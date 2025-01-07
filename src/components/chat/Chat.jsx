a13a9ca109556b6c9950efdda0e018693bf7337e
unknown <buiducthang1771996@gmail.com>
2025-01-07 17:01:10 +0700

    detail home

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,5 +1,19 @@
+import { useEffect, useRef, useState } from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 const Chat = () => {
+  const [openEmoji, setOpenEmoji] = useState(false);
+  const [text, setText] = useState("");
+  const endRef = useRef(null);
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
+  const handleEmoji = (event) => {
+    setText((prev) => prev + event.emoji);
+    setOpenEmoji(false);
+  };
+
   return (
     <>
       <div className="chat">
@@ -8,7 +22,7 @@ const Chat = () => {
             <img src="./avatar.png" alt="" />
             <div className="texts">
               <span>Bui Duc Thang</span>
-              <p>What are y doing, some time you need to hangout</p>
+              <p>Live for my life</p>
             </div>
           </div>
           <div className="icons">
@@ -17,11 +31,52 @@ const Chat = () => {
             <img src="./info.png" alt="" />
           </div>
         </div>
-        <div className="center"></div>
+        <div className="center">
+          <div className="message own">
+            <div className="texts">
+              <img src="./avatar.png" alt="" />
+              <p>
+                What are y doing, some time you need to hangout?, i got some
+                thing importand wont to telling you
+              </p>
+              <span>1 min ago</span>
+            </div>
+            <div ref={endRef}></div>
+          </div>
+          <div className="message">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+              <p>
+                What are y doing, some time you need to hangout?, i got some
+                thing importand wont to telling you
+              </p>
+              <span>1 min ago</span>
+            </div>
+          </div>
+        </div>
         <div className="bottom">
-          <div className="icons"></div>
-          <input type="text" placeholder="Type a message..." />
-          <div className="emoji"></div>
+          <div className="icons">
+            <img src="./img.png" alt="" />
+            <img src="./camera.png" alt="" />
+            <img src="./mic.png" alt="" />
+          </div>
+          <input
+            type="text"
+            placeholder="Type a message..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <div className="emoji">
+            <img
+              src="./emoji.png"
+              alt=""
+              onClick={() => setOpenEmoji((prev) => !prev)}
+            />
+            <div className="picker">
+              <EmojiPicker open={openEmoji} onEmojiClick={handleEmoji} />
+            </div>
+          </div>
+          <button className="sendButton">Send</button>
         </div>
       </div>
     </>
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,8 +1,51 @@
-import './detail.css'
+import "./detail.css";
 const Detail = () => {
-  return(
+  return (
     <>
-      <div className="detail">Detail</div>
+      <div className="detail">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <h2>Bui Duc Thang</h2>
+          <p>i alway try to be better</p>
+        </div>
+        <div className="info">
+          <div className="option">
+            <div className="title">
+              <span>Privacy & help</span>
+              <img src="./arrowUp.png" alt="" />
+            </div>
+          </div>
+          <div className="option">
+            <div className="title">
+              <span>Share photos</span>
+              <img src="./arrowDown.png" alt="" className="icon" />
+            </div>
+            <div className="photos">
+              <div className="photoItem">
+                <div className="photoDetail">
+                  <img
+                    width={300}
+                    src="https://images.hdqwalls.com/download/dota-2-game-art-2020-ld-1920x1080.jpg"
+                    alt=""
+                  />
+                  <span>photo_2024.png</span>
+                </div>
+
+                <img src="download.png" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="option">
+            <div className="title">
+              <span>Share Files</span>
+              <img src="./arrowUp.png" alt="" />
+            </div>
+          </div>
+
+          <button>Block User</button>
+          <button className="logout">Logout</button>
+        </div>
+      </div>
     </>
   );
 };
